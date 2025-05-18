fb0c491363df59f6036e66ba0ae02d244a00ded1
unknown <femiduyile@gmail.com>
2025-05-18 08:21:16 +0100

    'order'

--- araile/settings.py
+++ araile/settings.py
@@ -92,17 +92,17 @@ WSGI_APPLICATION = 'araile.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 print("DEBUG:", DEBUG)
 # New commit
-# external = "postgresql://postgres:hIGDdcgzpMMlGqBrnImSXdqNnwJlvhjh@shuttle.proxy.rlwy.net:43743/railway"
-# DATABASES = {
-#     'default': dj_database_url.parse(external)
-# }
-
+external = "postgresql://postgres:hIGDdcgzpMMlGqBrnImSXdqNnwJlvhjh@shuttle.proxy.rlwy.net:43743/railway"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(external)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
